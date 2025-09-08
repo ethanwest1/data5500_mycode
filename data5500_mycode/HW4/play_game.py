0aa866763d6c2408c8002f36943d9a032afc2b55
root <root@ip-172-31-84-157.ec2.internal>
2025-09-08 19:47:02 +0000

    Worked on HW4

--- data5500_mycode/HW4/play_game.py
+++ data5500_mycode/HW4/play_game.py
@@ -2,7 +2,24 @@
 
 from DeckOfCards import *
 
-def welcome():
+#Score hand helper
+def score_hand(hand_cards):
+    #Counters to track hand total and num of aces
+    total = 0
+    ace_count = 0
+    for card in hand_cards:
+        total += card.val
+        if card.face == 'Ace': ###### I'M NOT SURE IF CARD.FACE is the correct reference. 
+            ace_count += 1
+
+
+def is_bust():
+    return
+
+
+
+#Main function to play the game. Contains: welcome, dealing to the user, gameplay
+def main():
     #Welcome Message
     print("\nWelcome to Blackjack! I hope you're feeling lucky!\n")
 
@@ -16,6 +33,23 @@ def welcome():
     print("\nDeck After Shuffled:")
     deck.print_deck()
 
-welcome()
+    #Deal 2 cards to the user + print the cards. 
+    card1 = deck.get_card()
+    card2 = deck.get_card()
+    
+    print(f"\nCard 1 is: {card1}")
+    print(f"Card 2 is: {card2}")
 
-def 
+    #Calculate + print the score of the user's hand
+    score = 0
+    score += card1.val
+    score += card2.val
+
+    print(f"Your score is: {score}")
+
+    #User Gameplay (hit or not)
+    hit = input("\nWould you like to hit? (y/n) ")
+
+main()
+
+

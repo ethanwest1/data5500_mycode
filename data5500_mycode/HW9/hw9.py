d304b1fbfed74abd39df0e6dc6daffc1a1ffe650
root <root@ip-172-31-84-157.ec2.internal>
2025-11-20 06:01:46 +0000

    Tweaked disclaimer

--- data5500_mycode/HW9/hw9.py
+++ data5500_mycode/HW9/hw9.py
@@ -5,8 +5,9 @@
 ### DISCLAIMER
 #I'm fully aware that my code doesn't print out any info for the ada coin. We tried to incorporate the reverse path logic into the arbitrage detection logic
 # and you can see that it works with all coins except for ada. That is because ada has a forward path, but for some reason won't return a reverse path. 
-#ChatGPT and I worked to debug this, but we weren't able to figure it out. Rather than stripping my code of the reverse logic just so the output looks
-#nicer, I decided to include the bug rather than throwing away all of the work we put into it. So I hope that you can understand where I'm coming from. 
+#ChatGPT and I worked to debug this, but we weren't able to figure it out. We added code that allows the program to continue when this error is encountered. 
+#Rather than stripping my code of the reverse logic just so the output looks nicer, I decided to include the bug rather than throwing away all of the work 
+#we put into it. So I hope that you can understand where I'm coming from. 
 #This assignment was tricky, but ChatGPT helped walk me through it, and I can confidently say that I understand what is happening and how the program works. 
 
 
